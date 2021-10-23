3ef6842c64af14d668d889de42edc16b87c85066
Adhnan Asharaf <adhnanasharaf@Adhnans-Air.domain.name>
2021-10-23 12:51:54 +0530

    letmec finished

--- task-4/NASA.py
+++ task-4/NASA.py
@@ -1,19 +1,22 @@
 import requests
 import json
 
-from requests.models import Response
+
 
 date = input()
 rovername = input("a")
-def req(date,rovername):
+api_key = "mn45IIPQlKrN2AlyuwaJS1SCPeIPC4yuc56i4pC0"
+url = "https://api.nasa.gov/mars-photos/api/v1/rovers/Curiosity/photos/img_src"
+def req(date,rovername,url,api_key):
     if(date,rovername):
         # print(rovername)
-        response = requests.get("https://api.nasa.gov/mars-photos/api/v1/rovers/"+rovername+"/photos?earth_date="+date+"&api_key=mn45IIPQlKrN2AlyuwaJS1SCPeIPC4yuc56i4pC0")
+        response = requests.get(url,params={'earth_date':date,'api_key':api_key,'rover':rovername})
     else:
         print("no data")
 
-    print(response.status_code)
-    print(response.json())
+   
+    wjdata = response.json()
+    print(wjdata[0])
 
 
-req(date,rovername)    
+req(date,rovername,url,api_key)    
